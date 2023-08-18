16e168aa143ea23c4afe69e84c63986f1c59554a
Meet Satra <meetvsatra@gmail.com>
2023-08-18 12:06:51 +0530

    exp5 part 1 and 2

---
+++ exp5_2(n-grams with nltk).py
@@ -0,0 +1,19 @@
+import nltk
+from nltk import ngrams, word_tokenize
+
+text = "NLP is my favourite subject"
+
+# nltk.download('punkt')  
+
+def generate_ngrams(text, n):
+    tokens = word_tokenize(text)
+    ngram_list = list(ngrams(tokens, n))
+    return [' '.join(ngram) for ngram in ngram_list]
+
+unigrams = generate_ngrams(text, 1)
+bigrams = generate_ngrams(text, 2)
+trigrams = generate_ngrams(text, 3)
+
+print("Unigrams:", unigrams)
+print("Bigrams:", bigrams)
+print("Trigrams:", trigrams)
